5e9ec920a66ed163a99584f95a3ef56851fc522c
Marti570 <martina.sanchez.garcias@mi.unc.edu.ar>
2026-05-21 10:34:37 -0300

    feat: Comienzo implementación Hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -9,6 +9,24 @@ export function Hero(){
             <div className="background_hero__opacity"></div>
             <div className="background_hero__gradient"></div>
 
+            <div className="hero__img_wrapper">
+                <img src="../public/imgs/hero-bg.jpg" alt="" className="hero__img_background" />
+            </div>
+            
+            <div className="hero__content">
+                <div className="hero__tag glass">
+                    <div></div>
+                    <p>Disponible para nuevos proyectos</p>
+                </div>
+
+                <h1 className="hero__title">
+                    <span className="name">José I. Tomé.</span>
+                    <span className="role">Frontend Developer</span>
+                </h1>
+
+                <p className="hero__subtitle">Construyo interfaces rápidas, accesibles y con detalles que importan.</p>
+
+            </div>
         </section>
 
     )
